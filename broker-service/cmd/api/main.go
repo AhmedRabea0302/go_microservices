c095560303d5916b296e842e9fb1fa2bc1aa011b
AhmedRabea0302 <arabea169@gmail.com>
2024-11-07 14:19:58 +0200

    Added Broker Service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting Broker Service on Port: %s\n", webPort)
+
+	// Define http Server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// Start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
